ce07f34c8618b172cded93fdf4d1e6afbdd94467
James Halliday <mail@substack.net>
2013-07-29 14:52:36 -0700

    4-dimensional example

--- example/multi.js
+++ example/multi.js
@@ -1,17 +1,13 @@
 var search = require('../');
-var q = search([ [ 0, 5 ], [ 0, 5 ] ], function (pt, cb) {
-    var x = pt[0], y = pt[1];
-    cb(
-        Math.sin(5 * x) - Math.cos(x)
-        + Math.sin(3 * y) - 1 / 5 * Math.cos(x * y + 3 * y)
-        + 1/4 * Math.sin(x - 1) - 2 * Math.cos(x)
-    );
-});
+var domain = [ [ -5, 5 ], [ -5, 5 ], [ -5, 5 ], [ -5, 5 ] ];
 
-var count = 0;
-q.on('test', function (pt, x) {
-    console.log('TEST', pt, x);
-    if (count++ > 20) q.stop();
+var q = search(domain, function (pt, cb) {
+    var x = pt[0], y = pt[1], z = pt[2], w = pt[3];
+    cb(
+        Math.sin(x + y * Math.cos(z) / 10)
+        + Math.sin(3 * y) - Math.cos(z + x) * 1/2
+        + Math.sin(5 * w) * Math.sin(3 * x + 4 * w) * 1/8
+    );
 });
 
 q.on('max', function (pt, x) {
