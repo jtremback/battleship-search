dc2eaebb4b1702c3c9aaedff58d5f60c2ca89f20
James Halliday <mail@substack.net>
2013-07-29 18:29:33 -0700

    only show the found MAX values in the output

--- example/search.js
+++ example/search.js
@@ -7,15 +7,8 @@ var q = search([ [ 0, 5 ] ], function (pt, cb) {
     );
 });
 
-var count = 0;
-q.on('test', function (pt, x) {
-    console.log('TEST', pt, x);
-    if (count++ > 50) q.stop();
-});
-
 q.on('max', function (pt, x) {
     console.log('MAX', pt, x);
-    if (count++ > 50) q.stop();
 });
 
 q.start();
