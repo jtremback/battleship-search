7034e5bc229bcf7806475580330dddb627c00737
James Halliday <mail@substack.net>
2014-03-12 17:17:31 -0700

    better function

--- example/bounds/main.js
+++ example/bounds/main.js
@@ -12,9 +12,9 @@ var q = search([ [ 0, 5 ], [ 0, 5 ] ], function (pt, cb) {
     var x = pt[0], y = pt[1];
     pending.push(function () {
         cb(
-            Math.sin(5 * x) - Math.cos(x)
-            + Math.sin(3 * y) - 1 / 5 * Math.cos(x * y + 3 * y)
-            + 1/4 * Math.sin(x - 1) - 2 * Math.cos(x)
+            Math.sin(5 * x + 3) - Math.cos(x)
+            + Math.sin(3 * y - 4) - 1 / 5 * Math.cos(x * y + 3 * y)
+            + 1/4 * Math.sin(3 * x - 1) - 2 * Math.cos(x)
         );
     });
 });
