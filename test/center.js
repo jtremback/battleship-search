774908e205cc749c66fece8d8a1ebc4129de4e78
James Halliday <mail@substack.net>
2014-03-16 18:15:48 -0600

    failing test for 4d center

---
+++ test/center.js
@@ -0,0 +1,19 @@
+var center = require('../lib/center.js');
+var test = require('tape');
+
+test('circumscribed center', function (t) {
+    var pt2 = center([ [ 0, 5 ], [ 2.5, 2.5 ], [ 5, 5 ] ]);
+    t.deepEqual(pt2, [ 2.5, 5 ], '2d');
+    
+    var pt4 = center([
+        [ 0, 0, 0, 0 ],
+        [ -5, -5, -5, -5 ],
+        [ -5, -5, -5, 5 ],
+        [ -5, -5, 5, 5 ],
+        [ -5, -5, 5, -5 ]
+    ]);
+    console.log(pt4);
+    t.ok(pt4, '4d');
+    
+    t.end();
+});
