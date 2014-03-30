ff071ed57dd7013ac53041b0e0a78ad7060265ff
James Halliday <mail@substack.net>
2014-03-29 21:54:34 -0700

    center point not needed anymore

--- example/multi.js
+++ example/multi.js
@@ -4,8 +4,8 @@ var domain = [ [ -5, 5 ], [ -5, 5 ], [ -5, 5 ], [ -5, 5 ] ];
 var q = search(domain, function (pt) {
     var x = pt[0], y = pt[1], z = pt[2], w = pt[3];
     return Math.sin(x + y * Math.cos(z) / 10)
-        + Math.sin(3 * y) - Math.cos(z + x) * 1/2
-        + Math.sin(5 * w) * Math.sin(3 * x + 4 * w) * 1/8
+        + Math.sin(3 * (y - 3)) - 7 * Math.cos((z-1) + x) * 1/2
+        + 2 * Math.sin(5 * (w - 5)) * Math.sin(3 * x + 4 * w) * 1/8
     ;
 });
 
@@ -14,5 +14,6 @@ q.on('max', function (pt, x) {
 });
 
 while (true) {
-    console.log(q.next());
+    //console.log(q.next());
+    q.next();
 }
